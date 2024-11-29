83a4782c539e16c6ffdc339ed809d4cf51e82132
chavda-parth <chavdaparth960@gmail.com>
2024-11-29 13:17:10 +0530

    Minor change.

--- src/passgen/PasswordGenerator.java
+++ src/passgen/PasswordGenerator.java
@@ -9,7 +9,7 @@ public class PasswordGenerator {
         System.out.println("WELCOME TO GLITCH'S PASSWORD GENERATOR");
         System.out.println("LETS GET YOU A NEW PASSWORD");
 
-        acc = new Account("Google", "chavdaparth480@gmail.com", 8, 4, 1, 1, 1);
+        acc = new Account("Google", "xyz123@gmail.com", 8, 4, 1, 1, 1);
 
         System.out.println(acc.toString());
 
